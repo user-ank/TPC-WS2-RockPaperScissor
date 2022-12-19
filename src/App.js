e7aecced00b5f20b3a803ef89cba158b7f9bfcfc
user-ank <97175066+user-ank@users.noreply.github.com>
2022-12-19 16:51:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ShowRule from './components/ShowRule';
+import Body from './components/Body';
+
+
+function App() {
+	
+	
+	return (
+		<div>
+			<Body/>
+			<ShowRule/>
+		</div>
+	);
+}
+
+export default App;
